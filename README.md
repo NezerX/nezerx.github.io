c203e0e9f312ec05f5edd5955f9086515eed053d
PC_Lenovo_020 <PC_Lenovo_020@lider.lider>
2017-10-02 17:08:19 +0500

    Update

--- README.md
+++
@@ -1,1 +0,0 @@
-# nezerx.github.io
---
+++ js/main.js
@@ -0,0 +1,5 @@
+var scroll = new SmoothScroll();
+
+var about = document.getElementById('about')
+var projects = document.getElementById('projects')
+var contacts = document.getElementById('contacts')
