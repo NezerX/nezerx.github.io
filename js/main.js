c32741c8de634b17b6bf98507864c06eda51b161
PC_Lenovo_020 <PC_Lenovo_020@lider.lider>
2017-10-03 16:54:49 +0500

    Fix navigation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var scroll = new SmoothScroll();
 
-var about = document.getElementById('about')
-var projects = document.getElementById('projects')
-var contacts = document.getElementById('contacts')
+var about = document.getElementById('about');
+var projects = document.getElementById('projects');
+var contacts = document.getElementById('contacts');
