90208a36db43809375efc0bed8bd5ba63fda2509
PC_Lenovo_020 <PC_Lenovo_020@lider.lider>
2017-10-05 17:15:21 +0500

    Design update

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@ var scroll = new SmoothScroll();
 var about = document.getElementById('about');
 var projects = document.getElementById('projects');
 var contacts = document.getElementById('contacts');
+
+$('button').click(function() {
+$('section').toggleClass('animated fadeInLeft');
+});
